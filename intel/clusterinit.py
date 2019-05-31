c9e5f057e7ad48c917c580beece0c04d3b3194ae
Chris Friesen <chris.friesen@windriver.com>
2019-05-31 13:05:50 -0600

    Make webhook tolerations specific rather than unconditional
    The CMK webhook pod is currently running with unconditional toleration
    and also injects unconditional toleration into arbitrary worker pods
    that request CMK resources.
    
    This is a bug, as it means that locking a node does not cause the
    worker pods to be evicted.
    
    The fix is to make the tolerations more strict such that they only
    tolerate the taint added by CMK.
    
    Signed-off-by: Chris Friesen <chris.friesen@windriver.com>

--- intel/clusterinit.py
+++ intel/clusterinit.py
@@ -430,7 +430,7 @@ def update_pod_with_webhook_container(pod, cmk_img, configmap_name,
             'secretName': secret_name
          }
     })
-    pod["spec"]["tolerations"] = [{"operator": "Exists"}]
+    pod["spec"]["tolerations"] = [{"operator": "Exists", "key": "cmk", "effect": "NoSchedule"}]
     pod["spec"]["containers"].append(container)
     pod["spec"].pop("nodeName")
 
@@ -508,6 +508,8 @@ def get_default_webhook_mutations_config():
                     "serviceAccount": "cmk-serviceaccount",
                     "tolerations": [
                         {
+                            "key": "cmk",
+                            "effect": "NoSchedule",
                             "operator": "Exists"
                         }
                     ],
